2e3c3cdd830993dc56b01e09b713ef351565d09a
Naser Papi <naser.papi.dev@gmail.com>
2024-05-08 16:17:22 +0330

    just test new CICD configs for the azure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
-          Welcome to my naser papi website!
+          Welcome to naser papi website!
       </h1>
     </main>
   );
