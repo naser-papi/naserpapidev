db17d10f244dc5ad259dfe4cba58f986e747033c
Naser Papi <naser.papi.dev@gmail.com>
2024-08-30 00:25:01 +0330

    changed some texts of hero section

--- src/components/atom/label-icon/index.tsx
+++ src/components/atom/label-icon/index.tsx
@@ -1,21 +1,31 @@
+import Link from "next/link";
+
 interface IconLabelProps {
     icon: JSX.Element;
     label: string;
+    link?: string;
 }
 
-const IconLabel = ({ icon, label }: IconLabelProps) => {
-    return (
-        <div className={"flex items-center gap-2 text-secondary-900"}>
+const IconLabel = ({ icon, label, link }: IconLabelProps) => {
+    const className = `flex items-center gap-2 text-secondary-900`;
+    const children = (
+        <>
             {icon}
             <label
-                className={
-                    "font-ibm text-primary-100 text-sm font-normal sm:text-lg"
-                }
+                className={`font-ibm text-primary-100 text-sm font-normal sm:text-lg ${link ? "cursor-pointer" : ""}`}
             >
                 {label}
             </label>
-        </div>
+        </>
     );
+    if (link) {
+        return (
+            <Link href={link} className={className}>
+                {children}
+            </Link>
+        );
+    }
+    return <div className={className}>{children}</div>;
 };
 
 export default IconLabel;
--- src/components/organism/landing/hero-box.tsx
+++ src/components/organism/landing/hero-box.tsx
@@ -13,29 +13,33 @@ const HeroBox = () => {
                 <div className={"flex flex-col gap-3 items-center sm:text-2xl"}>
                     <Avatar src={avatar} alt={"naser papi"} />
                     <h3>Naser Papi</h3>
-                    <strong>Senior Web Developer</strong>
+                    <strong>Senior Javascript Developer</strong>
                 </div>
-                <div className={"flex flex-col gap-3 sm:gap-6"}>
+                <div className={"flex flex-col gap-3 sm:gap-4 pl-4"}>
                     <IconLabel
                         icon={<i className={"fa-solid fa-envelope"} />}
                         label={"naserpapi@hotmail.com"}
+                        link={"mailto:naserpapi@hotmail.com"}
                     />
                     <IconLabel
-                        icon={<i className="fa-solid fa-location-dot" />}
-                        label={"Iran, Tehran"}
+                        icon={<i className="fa-regular fa-crown" />}
+                        label={"Fan of JavaScript Anywhere"}
+                    />
+
+                    <IconLabel
+                        icon={<i className="fa-regular fa-graduation-cap" />}
+                        label={"Education: BD of software Engineering"}
                     />
                     <IconLabel
-                        icon={<i className="fa-solid fa-display-code" />}
-                        label={"Full time - freelancer"}
-                    />
-                    <IconLabel
-                        icon={<i className="fa-solid fa-globe-pointer" />}
-                        label={"https://www.naserpapi.dev"}
+                        icon={<i className="fa-regular fa-heart" />}
+                        label={
+                            "Teamwork, Leadership, Creativity, Problem-Solving"
+                        }
                     />
                     <div className={"flex items-center gap-2"}>
                         <Tag text={"React"} />
-                        <Tag text={"TypeScript"} />
-                        <Tag text={"NextJs"} />
+                        <Tag text={"NeXt.JS"} />
+                        <Tag text={"NeSt.JS"} />
                         <Tag text={"MongoDB"} />
                     </div>
                 </div>
